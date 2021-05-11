18dddd79afbfc31f1c6102b2df9c48b0b3bdb78b
zsoldaat <zac.soldaat@gmail.com>
2021-05-11 08:31:07 -0700

    Added some example categorical and continuous data that will be used for the ChartTypeSelectionView Started working on a ChartTypeSelectionView with actual chart examples

--- Datum/View/Visualize Data/ChartTypeSelectionView.swift
+++ Datum/View/Visualize Data/ChartTypeSelectionView.swift
@@ -13,12 +13,40 @@ struct ChartTypeSelectionView: View {
     
     @Binding var visualizationManager: VisualizationManager
     
+    @State private var selection: Chart.ChartType = .barchart
+    
     var body: some View {
-        Picker("Chart Type", selection: $visualizationManager.chart.type) {
-            ForEach(Chart.ChartType.allCases) { chart in
-                Text(chart.rawValue.capitalized)
+//        Picker("Chart Type", selection: $visualizationManager.chart.type) {
+//            ForEach(Chart.ChartType.allCases) { chart in
+//                Text(chart.rawValue.capitalized)
+//            }
+//        }
+        
+        List {
+            
+            VStack {
+                BarchartView(categoricalVariable: CategoricalVariableStorage.exampleCategoricalVariable)
+                    .frame(height: 200)
+                Text("Barchart")
             }
+            .onTapGesture { self.selection = .barchart }
+            
+            VStack {
+                ScatterplotView(xvar: ContinuousVariableStorage.exampleVariables.first, yvar: ContinuousVariableStorage.exampleVariables.last)
+                    .frame(height: 200)
+                Text("Scatterplot")
+            }
+            
+            
+            
+            
+            
+            
+            
+            
         }
+        
+        
         
         DoneButton{presentationMode.wrappedValue.dismiss()}
         
